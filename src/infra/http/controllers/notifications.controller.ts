f181ae426d1e9edeb2354ba6d8e5c7e6289fcae1
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-08 12:15:19 -0300

    feat: WIP notifications

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from '../../../app/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotifications: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+    const { notification } = await this.sendNotifications.execute({
+      category,
+      content,
+      recipientId,
+    });
+
+    return { notification };
+  }
+}
