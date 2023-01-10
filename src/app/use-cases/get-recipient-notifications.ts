801363bd8c52c271c068c03a3ebd611c559b9703
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-10 06:42:27 -0300

    feat: create method to get notification by recipient id

---
+++ src/app/use-cases/get-recipient-notifications.ts
@@ -0,0 +1,25 @@
+import { Notification } from '@app/entities/notification';
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface GetRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationsResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute({
+    recipientId,
+  }: GetRecipientNotificationsRequest): Promise<GetRecipientNotificationsResponse> {
+    const notifications =
+      await this.notificationsRepository.findManyByRecipientId(recipientId);
+
+    return { notifications };
+  }
+}
