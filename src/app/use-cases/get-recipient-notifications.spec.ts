e1667d7930d7f4a99dabe23a77b65c6e6c0f5786
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-10 06:43:41 -0300

    test: add tests

---
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,36 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Get recipient notifications', () => {
+  it('should be able to count recipient notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countrecipientsNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'example-recipient-id' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'example-recipient-id' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'example-recipient-id-different' }),
+    );
+
+    const { notifications } = await countrecipientsNotifications.execute({
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'example-recipient-id' }),
+        expect.objectContaining({ recipientId: 'example-recipient-id' }),
+      ]),
+    );
+  });
+});
