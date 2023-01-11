3e4108928638a231a84c3553299a79f949c44e98
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-11 06:21:33 -0300

    feat: add method toDomain to Prisma

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
 import { Notification } from '@app/entities/notification';
+import { NotificationContent } from '@app/entities/notification-content';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new NotificationContent(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
